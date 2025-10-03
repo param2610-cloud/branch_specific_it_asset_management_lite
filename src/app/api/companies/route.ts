0e2deed115b76acf86aaa4a3febf6bf658685a0e
Parambrata Ghosh <gpampa138@gmail.com>
2025-10-03 16:43:09 +0530

    refactor: Improve error handling and response formatting in GET companies API

--- src/app/api/companies/route.ts
+++ src/app/api/companies/route.ts
@@ -1,30 +1,31 @@
-import { verifyToken } from "@/lib/token/token";
-import { secretKeyFetch } from "@/lib/token/token";
 import { ApiDict } from "@/data/snipe_it_api/ApiDict";
-import { NextRequest } from "next/server";
+import { secretKeyFetch, verifyToken } from "@/lib/token/token";
+import { NextRequest, NextResponse } from "next/server"
 
-export async function GET(req: NextRequest) {
-    const token = req.cookies.get('accessToken')?.value;
-    if (!token) {
-        return new Response("No token found", { status: 401 });
-    }
-    const validation = verifyToken(token);
-    if (!validation) {
-        return new Response("Invalid token", { status: 403 });
-    }
-
-    const userData = await secretKeyFetch(token);
-    if (!userData || typeof userData !== 'object' || !userData.secret || !userData.locationId) {
-        return new Response("User data not found", { status: 403 });
-    }
-
-    const url = new URL(req.url);
-    const params = Object.fromEntries(url.searchParams);
-
-    const result = await ApiDict.getCompanies(userData.secret, params);
-    if (result.success) {
-        return new Response(JSON.stringify(result.data), { status: 200 });
-    } else {
-        return new Response(`API Error: ${result.error}`, { status: 500 });
+export async function GET(req:NextRequest,res:NextResponse){
+    try {
+        const token = req.cookies.get('accessToken')?.value;
+        if (!token) {
+            return new Response(JSON.stringify({message:"No token found"}), {status: 401});
+        }
+        const validateToken= verifyToken(token);
+        if (!validateToken) {
+            return new Response(JSON.stringify({message:"Invalid token"}), {status: 403});
+        }
+        const userData = await secretKeyFetch(token);
+        if(!userData || typeof userData!=='object' || !userData.secret || !userData.locationId){
+            return new Response(JSON.stringify({message:"User data not found"}), {status: 403});
+        }
+        const url = new URL(req.url);
+        const params = Object.fromEntries(url.searchParams);
+        const result = await ApiDict.getCompanies(userData.secret, params);
+        if (result.success) {
+            return new Response(JSON.stringify(result.data), {status: 200});
+        } else {
+            return new Response(JSON.stringify({message:`API Error: ${result.error}`}), {status: 500});
+        }
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({message:"Internal server error"}), {status: 500});
     }
 }
