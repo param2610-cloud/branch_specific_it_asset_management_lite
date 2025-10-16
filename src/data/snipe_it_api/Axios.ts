bfbbc3f8bbef1aa1236b4c2fa416e4e3a2b0883d
Parambrata Ghosh <gpampa138@gmail.com>
2025-10-16 13:20:34 +0530

    feat: Add CORS support and update Axios timeout to 20 seconds

--- src/data/snipe_it_api/Axios.ts
+++ src/data/snipe_it_api/Axios.ts
@@ -9,6 +9,6 @@ export function createSnipeItAxios(secret: string): AxiosInstance {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
     },
-    timeout: 20000, // 10 seconds
+    timeout: 20000, // 20 seconds
   });
 }
